69465a5beeb22d7da59508e522a7cc453712b5ee
wei_jc <weijiancai@126.com>
2014-02-02 18:02:41 +0800

    meta servlet

---
+++ core/src/main/java/com/meteorite/core/web/MetaServlet.java
@@ -0,0 +1,32 @@
+package com.meteorite.core.web;
+
+import com.alibaba.fastjson.JSON;
+import com.meteorite.core.meta.MetaManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author wei_jc
+ * @since 1.0.0
+ */
+public class MetaServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        this.doPost(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        String metaName = req.getParameter("name");
+        writeJsonObject(res, MetaManager.getMeta(metaName));
+    }
+
+    private void writeJsonObject(HttpServletResponse res, Object obj) throws IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        res.getWriter().write(JSON.toJSONString(obj));
+    }
+}
