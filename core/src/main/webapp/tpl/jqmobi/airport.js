667c72b8e0bb83b60cd11f8877d751cb243bf2d4
wei_jc <weijiancai@126.com>
2015-01-05 06:31:35 +0800

    airport

--- core/src/main/webapp/tpl/jqmobi/airport.js
+++ core/src/main/webapp/tpl/jqmobi/airport.js
@@ -1,3 +1,19 @@
-function loadAirTicket() {
-    //alert('loadAirTicket');
-}
+$.ui.ready(function () {
+    var startLocation = $('#startLocation').find('h3');
+    var endLocation = $('#endLocation').find('h3');
+
+    // 交换
+    $('#changeLocation').click(function() {
+        var temp = startLocation.text();
+        startLocation.text(endLocation.text());
+        endLocation.text(temp);
+    });
+    // 出发地
+    $('#startLocation, #endLocation').click(function () {
+
+    });
+
+    $('#panel_Location').find('a').click(function () {
+        $.ui.loadContent('#airTicket', false, false);
+    });
+});
